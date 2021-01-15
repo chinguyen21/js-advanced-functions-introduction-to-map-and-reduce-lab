4951bd64516efa2aeb366103d92c1010f7951d84
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-01-15 11:20:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+const mapToNegativize = (sourceArray) => sourceArray.map(ele => -ele)
+
+const mapToNoChange = (array) => array.map(ele => ele)
+
+const mapToDouble = (array) => array.map(ele => ele * 2)
+
+const mapToSquare = (array) => array.map(ele => ele ** 2)
+
+// console.log(mapToSquare([1, 2, 3, -9]))
+
+
+const reduceToTotal = (arr, num=0) => arr.reduce((total, ele) => ele + total, num)
+
+const reduceToAllTrue = (arr) => arr.map(ele => ele ? true:false).every(ele => ele === true)
+
+
+const reduceToAnyTrue = (arr) => arr.some(ele => ele === true)
